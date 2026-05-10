f5696aa3907c48c6726aec26834a978181f3c467
Sunny kumar <155979106+sunnykr3@users.noreply.github.com>
2026-05-10 16:14:55 +0530

    Analytics Setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {App} from './App.jsx'
+import ReactGA  from "react-ga4"
+
+ReactGA.initialize("G-PRNFT85NKK");
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
